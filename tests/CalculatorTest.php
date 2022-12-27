7feae083a8d7f74d85471fef47f013f0ffb5c3d7
Nampoina <nampoina@qdata-mg.com>
2022-12-27 15:11:11 +0300

    build instable test unitaire KO

--- tests/CalculatorTest.php
+++ tests/CalculatorTest.php
@@ -4,7 +4,7 @@ class CalculatorTest extends \PHPUnit\Framework\TestCase {
     public function testAdd(){
 
         $calculator = new App\Calculator;
-        $result = $calculator->add(20,5);
+        $result = $calculator->add(15,5);
 
         
         $this->assertEquals(25,$result);
